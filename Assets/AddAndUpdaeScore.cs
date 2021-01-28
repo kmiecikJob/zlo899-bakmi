203286c454494a0483571b53216970f3554e237e
kmiecikJob <76965636+kmiecikJob@users.noreply.github.com>
2021-01-28 21:38:40 +0100

    Exaplme
    I'm tired a little bit today (had long day), so my explanation can be a little bit rusty

---
+++ Assets/AddAndUpdaeScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddAndUpdaeScore : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private SciptableInt scoreHolder;
+
+    private void Start()
+    {
+
+        text.text = scoreHolder.value.ToString();
+    }
+
+    public void IncreaseScore(int score)
+    {
+        scoreHolder.value += score;
+        text.text = scoreHolder.value.ToString();
+    }
+}
---
+++ Assets/SciptableInt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Thanks to this line you can create new Scriptable object that will hold only 1 int value
+//after creation it has different icon than rest of scripts, you CAN'T attach it to game object like normal scripts
+//but you can create scripta that can hold it
+[CreateAssetMenu (menuName ="ScriptableInt")]
+public class SciptableInt : ScriptableObject
+{
+    public int value;
+}
